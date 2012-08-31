e94889caacdde0fc9c6177082951eb6485cbb61e
Wu, Ke <wuke@cs.umd.edu>
2012-08-31 15:37:50 -0400

    a little doc

--- src/include/fst/extensions/pdt/paren-data.h
+++ src/include/fst/extensions/pdt/paren-data.h
@@ -1,7 +1,7 @@
 // paren-data.h
 //
 // \file
-// Metadata of parentheses
+// Data structure that provides parenthesis pairing information.
 
 #ifndef FST_EXTENSIONS_PDT_PAREN_DATA_H__
 #define FST_EXTENSIONS_PDT_PAREN_DATA_H__
@@ -31,6 +31,7 @@ class PdtParenData {
   typedef typename Arc::StateId StateId;
   typedef typename Arc::Weight Weight;
 
+  // Arc and its source state
   struct FullArc {
     FullArc(StateId s, const Arc &a): state(s), arc(a) {}
     StateId state;
@@ -42,8 +43,7 @@ class PdtParenData {
 
     struct Hash {
       size_t operator()(const FullArc &fa) {
-        size_t value = 0x345678;
-        value = 1000003 * value ^ fa.state;
+        size_t value = fa.state;
         value = 1000003 * value ^ fa.arc.ilabel;
         value = 1000003 * value ^ fa.arc.olabel;
         value = 1000003 * value ^ fa.arc.nextstate;
@@ -53,6 +53,7 @@ class PdtParenData {
     };
   };
 
+  // Iterator through full arcs
   class Iterator {
    public:
     bool Done() const {
@@ -68,36 +69,56 @@ class PdtParenData {
     }
 
    private:
-    typedef typename unordered_set<FullArc, typename FullArc::Hash>::const_iterator SetIter;
+    typedef typename ArcSet::const_iterator SetIter;
     Iterator() : at_(), end_() {}
     Iterator(SetIter begin, SetIter end) :
         at_(begin), end_(end) {}
     SetIter at_, end_;
   };
 
+  // The constructor does not initialize the mapping; Init() must also
+  // be called.
   PdtParenData(const Fst<Arc> &ifst, const vector<pair<Label, Label> > &parens) :
       ifst_(ifst.Copy()), parens_(parens), clear_(false) {}
 
+  // Inits to naive mapping, i.e. stores all opening/closing arcs by
+  // their open paren. This must be called before accessing any paren
+  // information.
   void Init() {
     AssignParenId();
     NaiveMapping();
   }
 
+  // Finds the open paren of a given label; if the label is a paren,
+  // its open paren label is returned; otherwise kNoLabel is
+  // returned. Useful for both normalizing paren identification and
+  // testing the kind of a label. Must be called after Init().
+  //
+  // To test if a label is
+  // - An open paren, use (label != kNoLabel && OpenParenId(label) == label);
+  // - A close paren, use (label != kNoLabel && OpenParenId(label) != kNoLabel && OpenParenId(label) != label);
+  // - A lexical paren, use (label != kNoLabel && OpenParenId(label) == label).
   Label OpenParenId(Label l) const {
     typename unordered_map<Label, Label>::const_iterator it = open_paren_.find(l);
     if (it == open_paren_.end())
       return kNoLabel;
     else
-      return *it;
+      return it->second;
   }
 
+  // Reports a pair of matching paren arcs. After ClearNaive() these
+  // will be used to provide more accurate answer to open/close arc
+  // queries.
   void ReportUseful(StateId open_src, const Arc &open_arc,
                     StateId close_src, const Arc &close_arc) {
-    Label open_paren = OpenParenId(open_arc.label);
+    Label open_paren = OpenParenId(open_arc.ilabel);
     sure_open_map_[ParenState<Arc>(open_paren, close_src)].insert(FullArc(open_src, open_arc));
     sure_close_map_[ParenState<Arc>(open_paren, open_arc.nextstate)].insert(FullArc(close_src, close_arc));
   }
 
+  // Notifies that all useful pairs have been reported; thus
+  // PdtParenData can discard the naive mappings and start using the
+  // sure mappings.
   void ClearNaive() {
     if (!clear_) {
       naive_open_map_.clear();
@@ -106,6 +127,8 @@ class PdtParenData {
     }
   }
 
+  // Returns an Iterator through all open paren arcs reachable to
+  // close_src with a matching paren.
   Iterator FindOpen(Label open_paren, StateId close_src) const {
     if (clear_)
       return FindSure(open_paren, close_src, sure_open_map_);
@@ -113,6 +136,8 @@ class PdtParenData {
       return FindNaive(open_paren, naive_open_map_);
   }
 
+  // Returns an Iterator through all close paren arcs reachable from
+  // open_dest with a matching paren.
   Iterator FindClose(Label open_paren, StateId open_dest) const {
     if (clear_)
       return FindSure(open_paren, open_dest, sure_close_map_);
@@ -121,10 +146,11 @@ class PdtParenData {
   }
 
  private:
-  typedef unordered_map<Label, unordered_set<FullArc, typename FullArc::Hash> > NaiveMap;
-  typedef unordered_map<ParenState<Arc>, unordered_set<FullArc, typename FullArc::Hash>, typename ParenState<Arc>::Hash> SureMap;
+  typedef unordered_set<FullArc, typename FullArc::Hash> ArcSet;
+  typedef unordered_map<Label, ArcSet> NaiveMap;
+  typedef unordered_map<ParenState<Arc>, ArcSet, typename ParenState<Arc>::Hash> SureMap;
 
-  // Assigns paren id
+  // Maps each paren label to its open paren label
   void AssignParenId() {
     for (typename vector<pair<Label, Label> >::const_iterator it = parens_.begin();
          it != parens_.end(); ++it) {
